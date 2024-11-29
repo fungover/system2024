4cbde1e06854ad306ac840ada3b2507da774347f
Martin Blomberg <martin.blomberg@outlook.com>
2024-11-29 11:58:18 +0100

    Initial Commit

---
+++ src/test/java/org/fungover/system2024/System2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fungover.system2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class System2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
