2b3660006b2f9ae377a10226fa38c187db4f5fe2
Mats <matjoh50@gmail.com>
2024-12-02 21:56:02 +0100

    new base structure to seperate BE and FE, also installed React using Vite in FE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -29,9 +28,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <div className='bg-red-400'>
-        this is a test div
-      </div>
     </>
   )
 }
