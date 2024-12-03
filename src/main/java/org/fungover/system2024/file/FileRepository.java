cdb6384c2f281d616d8d478042736aa10937a1f4
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-12-03 16:21:12 +0100

    Switch to CrudRepository for compatibility.
    Replaced ListCrudRepository with CrudRepository in FileRepository because codereviewbot didnt like it since it does not exist in the standard Spring Data library

--- src/main/java/org/fungover/system2024/file/FileRepository.java
+++ src/main/java/org/fungover/system2024/file/FileRepository.java
@@ -1,10 +1,10 @@
 package org.fungover.system2024.file;
 
 import org.fungover.system2024.file.entity.File;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface FileRepository extends ListCrudRepository<File, Integer> {
+public interface FileRepository extends CrudRepository<File, Integer> {
     List<File> findByName(String name);
 }
