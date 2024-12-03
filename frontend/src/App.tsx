f74bb3870e1690d2aceeba39772ec95deadf7cc6
Mats <matjoh50@gmail.com>
2024-12-03 10:08:08 +0100

    added tailwind to frontend
    Signed-off-by: Mats <matjoh50@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,6 +29,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className='bg-red-400'>
+        this is a test div
+      </div>
     </>
   )
 }
