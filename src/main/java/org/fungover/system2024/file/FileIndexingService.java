8acc4d363c7d6250ec04166a8574f740e26a13f2
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-12-04 16:27:42 +0100

    Add FileIndexingService for entity mass indexing
    Introduces FileIndexingService to handle mass indexing of entities using Hibernate Search. Implements the ApplicationRunner interface to trigger indexing at application startup. This service supports starting a mass indexer for specified entity types and ensures search index is built without interrupting application transactions.

---
+++ src/main/java/org/fungover/system2024/file/FileIndexingService.java
@@ -0,0 +1,41 @@
+package org.fungover.system2024.file;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.fungover.system2024.file.entity.File;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.session.SearchSession;
+import org.hibernate.search.mapper.orm.massindexing.MassIndexer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionTemplate;
+
+@Service
+public class FileIndexingService implements ApplicationRunner {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        startIndexing(File.class);
+    }
+
+    public <T> void startIndexing(Class<T> entityType) {
+        transactionTemplate.executeWithoutResult(transactionStatus -> {
+            try {
+                SearchSession searchSession = Search.session(entityManager);
+                MassIndexer indexer = searchSession.massIndexer(entityType);
+                indexer.startAndWait();
+            } catch (InterruptedException e) {
+                System.out.println("An error occurred trying to build the search index: " + e.toString());
+            }
+        });
+    }
+}
