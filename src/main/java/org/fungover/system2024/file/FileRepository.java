78d146b0d65e8179e01a5c0b4b8ad8295f551431
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-12-04 16:38:41 +0100

    Remove commented out old fuzzy search logic from FileRepository

--- src/main/java/org/fungover/system2024/file/FileRepository.java
+++ src/main/java/org/fungover/system2024/file/FileRepository.java
@@ -1,14 +1,6 @@
 package org.fungover.system2024.file;
 
 import org.fungover.system2024.file.entity.File;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
-public interface FileRepository extends ListCrudRepository<File, Integer> {
-    /* Fuzzy Search, This will return results that are kinda like the search param
-    @Query("SELECT f FROM File f WHERE LOWER(f.name) LIKE LOWER(CONCAT('%',:name,'%'))")
-    List<File> findByNameFuzzy(@Param("name") String name);*/
-}
+public interface FileRepository extends ListCrudRepository<File, Integer> {}
