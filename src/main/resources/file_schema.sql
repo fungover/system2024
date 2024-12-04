7b9f0b51a50f7e3fe4953e61e18aa4d52945ff5c
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-12-04 22:13:12 +0100

    Enhance null check and database character settings
    Added a null check in the `FileDTO` constructor to prevent null files from causing errors. Updated `file_test_schema.sql` and `file_schema.sql` to ensure proper character encoding with utf8mb4 for better Unicode support. These changes improve error handling and database compatibility with diverse text data.

--- src/main/resources/file_schema.sql
+++ src/main/resources/file_schema.sql
@@ -1,6 +1,9 @@
 CREATE TABLE IF NOT EXISTS file(
-    id INT AUTO_INCREMENT
-        PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    file_url TEXT NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
+        CHARACTER SET utf8mb4
+            COLLATE utf8mb4_unicode_ci NOT NULL,
+    file_url TEXT
+        CHARACTER SET utf8mb4
+            COLLATE utf8mb4_unicode_ci NOT NULL
 )
