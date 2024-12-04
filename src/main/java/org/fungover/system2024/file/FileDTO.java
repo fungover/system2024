a4d9cbe6adeafdec8cad23eddc99b9d3c10bb330
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-12-04 22:13:12 +0100

    Enhance null check and database character settings
    Added a null check in the `FileDTO` constructor to prevent null files from causing errors. Updated `file_test_schema.sql` and `file_schema.sql` to ensure proper character encoding with utf8mb4 for better Unicode support. These changes improve error handling and database compatibility with diverse text data.

--- src/main/java/org/fungover/system2024/file/FileDTO.java
+++ src/main/java/org/fungover/system2024/file/FileDTO.java
@@ -4,17 +4,13 @@ import org.fungover.system2024.file.entity.File;
 
 public record FileDTO(String name, String fileUrl) {
     public static FileDTO fromFile(File file) {
-        return new FileDTO(
-                file.getName(),
-                file.getFileUrl()
-        );
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getFileUrl() {
-        return fileUrl;
+        if (file == null) {
+            System.out.println("file is null");
+            return null;
+        }
+            return new FileDTO(
+                    file.getName(),
+                    file.getFileUrl()
+            );
     }
 }
