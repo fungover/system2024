f89129b2bb91ef9e74e301a05c0581866f87a332
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-12-05 13:40:09 +0100

    Replace null checks with exceptions and handle config errors.
    Refactored FileDTO to throw IllegalArgumentException instead of returning null for null file inputs, enhancing error handling. Added try-catch block in CustomLuceneAnalysisConfigurer to capture and log exceptions during analyzer configuration. Removed unnecessary TRUNCATE statement from file_data.sql to prevent accidental data loss.

--- src/main/java/org/fungover/system2024/config/CustomLuceneAnalysisConfigurer.java
+++ src/main/java/org/fungover/system2024/config/CustomLuceneAnalysisConfigurer.java
@@ -6,8 +6,12 @@ import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurer;
 public class CustomLuceneAnalysisConfigurer implements LuceneAnalysisConfigurer {
     @Override
     public void configure(LuceneAnalysisConfigurationContext context) {
+        try {
         context.analyzer("text_analyzer").custom()
                 .tokenizer("standard")
                 .tokenFilter("lowercase");
+    } catch (Exception e) {
+            System.out.println("An error occurred trying to build the search index: " + e);
+        }
     }
 }
