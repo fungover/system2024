1b64d99de0de39e7d6d6f01dcc8848bc246c5138
AlbinNils98 <145000303+AlbinNils98@users.noreply.github.com>
2024-12-06 13:58:46 +0100

    Created entities and repositories for role and permission
    Also connected them to user entity

--- src/main/java/org/fungover/system2024/entities/User.java
+++ src/main/java/org/fungover/system2024/entities/User.java
@@ -1,18 +1,21 @@
 package org.fungover.system2024.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "users", schema = "system24db")
+@Table(name = "user", schema = "system24db")
 public class User {
     @Id
     @Column(name = "id", nullable = false)
@@ -30,6 +33,30 @@ public class User {
 
     @Size (max = 255)
     @NotNull
-    @Column(name = "email", nullable = false, unique = true)
+    @Column(name = "password", nullable = false, unique = true)
     private String password;
+
+    @ManyToMany
+    @JoinTable(name = "user_role",
+    joinColumns = @JoinColumn(
+    name = "user_id", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(
+        name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles = new LinkedHashSet<>();
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS users
+CREATE TABLE IF NOT EXISTS user
 (
     id       INT AUTO_INCREMENT PRIMARY KEY,
     name     VARCHAR(255) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS users
     password VARCHAR(255) NOT NULL
     );
 
-CREATE TABLE IF NOT EXISTS roles
+CREATE TABLE IF NOT EXISTS role
 (
     id   INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
@@ -17,22 +17,22 @@ CREATE TABLE IF NOT EXISTS user_role
     role_id INT,
     user_id INT,
     PRIMARY KEY (role_id, user_id),
-    FOREIGN KEY (role_id) REFERENCES roles (id),
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (user_id) REFERENCES user (id)
     );
 
-CREATE TABLE IF NOT EXISTS permissions
+CREATE TABLE IF NOT EXISTS permission
 (
     id          INT AUTO_INCREMENT PRIMARY KEY,
     name        VARCHAR(255) NOT NULL,
     description TEXT
     );
 
-CREATE TABLE IF NOT EXISTS role_permissions
+CREATE TABLE IF NOT EXISTS role_permission
 (
     permission_id INT,
     role_id       INT,
     PRIMARY KEY (permission_id, role_id),
-    FOREIGN KEY (permission_id) REFERENCES permissions (id),
-    FOREIGN KEY (role_id) REFERENCES roles (id)
+    FOREIGN KEY (permission_id) REFERENCES permission (id),
+    FOREIGN KEY (role_id) REFERENCES role (id)
     );
