3de4fcd0d4b1e73df6cee1546178721643748134
AlbinNils98 <145000303+AlbinNils98@users.noreply.github.com>
2024-12-09 12:26:36 +0100

    Changed max length of email and password columns as mentioned by reviewer. I was reluctant to set the password length to max 60 as i do not yet know which algorithm we will use to hash passwords, i assume it will be bcrypt then 60 is good.

--- src/main/java/org/fungover/system2024/entities/User.java
+++ src/main/java/org/fungover/system2024/entities/User.java
@@ -25,12 +25,12 @@ public class User {
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Size (max = 255)
+    @Size (max = 100)
     @NotNull
     @Column(name = "email", nullable = false)
     private String email;
 
-    @Size (max = 255)
+    @Size (max = 60)
     @NotNull
     @Column(name = "password", nullable = false, unique = true)
     private String password;
