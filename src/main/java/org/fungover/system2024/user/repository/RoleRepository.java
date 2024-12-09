aeb1a41fe8fa55ad74e38691d923f2998769df8c
AlbinNils98 <145000303+AlbinNils98@users.noreply.github.com>
2024-12-09 15:47:22 +0100

    Re-structured user

--- src/main/java/org/fungover/system2024/user/repository/RoleRepository.java
+++ src/main/java/org/fungover/system2024/user/repository/RoleRepository.java
@@ -1,6 +1,6 @@
-package org.fungover.system2024.repositories;
+package org.fungover.system2024.user.repository;
 
-import org.fungover.system2024.entities.Role;
+import org.fungover.system2024.user.entity.Role;
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface RoleRepository extends ListCrudRepository<Role, Integer> {
