a9c82aad6fe94ba617ba3f119a06eaa430352edc
JrSchoolAccount <jesperx2000@gmail.com>
2024-12-11 10:05:44 +0100

    Added Vite dependencies, react-dom and setup WebMvcConfigurer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    },
+    {
+        path: '/cake',
+        element: <div style={{ fontSize: 150 }}>🍰</div>,
+    },
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
