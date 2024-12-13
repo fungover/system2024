7c3270584feca1c11662511b3e06c287255e4797
Weeezzt <william.vesterberg@lerniastudent.se>
2024-12-13 12:58:51 +0100

    removed some imports
    Co-authored-by: AlexanderK88 <AlexanderK88@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Header from "./components/common/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="w-screen">
