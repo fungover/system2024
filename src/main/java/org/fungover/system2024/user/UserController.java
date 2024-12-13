c695f15d43e2b6f88ecbc726676441c4b7a790d3
AlbinNils98 <145000303+AlbinNils98@users.noreply.github.com>
2024-12-13 16:39:33 +0100

    Removed SLF4J annotation in controller

--- src/main/java/org/fungover/system2024/user/UserController.java
+++ src/main/java/org/fungover/system2024/user/UserController.java
@@ -1,11 +1,9 @@
 package org.fungover.system2024.user;
 
-import lombok.extern.slf4j.Slf4j;
 import org.fungover.system2024.user.dto.UserDto;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
-@Slf4j
 @Controller
 public class UserController {
 
