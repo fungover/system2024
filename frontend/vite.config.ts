4f9acc75e2b6078bc92678e5a26d4f378cdc6682
JrSchoolAccount <jesperx2000@gmail.com>
2024-12-13 08:26:41 +0100

    Added relative path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../src/main/resources/static/',
+    outDir: resolve(__dirname, 'src/main/resources/static/'),
     emptyOutDir: true,
   },
 });
