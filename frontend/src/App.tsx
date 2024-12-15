8b4b1276b81c394e16219e1431e13e60c35610e8
ConnyPettersson <pedalconny@hotmail.com>
2024-12-15 15:17:13 +0100

    Add a Tailwind-styled login page integrating GitHub OAuth2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,9 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 
+import LoginPage from './LoginPage'
+
 function App() {
-    return (
-        <div>
-            <h1>Welcome!</h1>
-            <p>Click the button below to sign in with GitHub</p>
-            <a href={"/oauth2/authorization/github"}>
-                <button>Sign in with GitHub</button>
-            </a>
-        </div>
-    );
+    return <LoginPage />;
 }
 
 export default App;
