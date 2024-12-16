8878473ad8e35461ab9ff16712572c08558d14bd
MikaelaMathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-12-16 10:24:33 +0100

    removing duplicates and unused import statements

--- src/main/java/org/fungover/system2024/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.fungover.system2024.repositories;
-
-import org.fungover.system2024.entities.User;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface UserRepository extends ListCrudRepository<User, Integer> {
-}
