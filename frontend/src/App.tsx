f5e4d926082e9ae23b860bb4c9c8a604644495c3
MickeMarten <123842543+MickeMarten@users.noreply.github.com>
2024-12-16 10:30:10 +0100

    Ci test.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+          {/*Hello this is a test to see the CI-changes.*/}
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
