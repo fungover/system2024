43b92f56bf27a93ded57b25499be7bbe9a829b26
Weeezzt <william.vesterberg@lerniastudent.se>
2024-12-17 15:25:27 +0100

    Created a responisve footer
    Co-authored-by: AlexanderK88 <AlexanderK88@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
 import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
 
 function App() {
   return (
     <>
-      <div className="w-screen">
+      <div className="w-full">
         <Header />
         <main className="w-full">
           <div className="min-h-screen w-full">
             <h1 className="my-40 text-center">WEBPAGE</h1>
           </div>
         </main>
-        <footer> footer </footer>
+        <Footer />
       </div>
     </>
   );
---
+++ frontend/src/components/common/Footer.tsx
@@ -0,0 +1,55 @@
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <div className="flex sm:flex-row flex-col w-full min-h-52 bg-gray-800 text-white pl-10 space-y-6 sm:space-y-0 sm:justify-center sm:gap-8 md:gap-16 lg:gap-24 xl:gap-32 2xl:gap-40 3xl:gap-48 py-10">
+      <div className="min-[520px]:flex min-[520px]:justify-between min-[520px]:pr-16 sm:pr-0 sm:gap-8 md:gap-16 lg:gap-24 xl:gap-32 2xl:gap-40 3xl:gap-48">
+        <div className="mb-5 sm:mb-0">
+          <h2 className="text-xl font-bold mb-2">Navigering</h2>
+          <ul className="">
+            <a href="/">
+              <li>Home</li>
+            </a>
+            <a href="/">
+              <li>About</li>
+            </a>
+            <a href="/">
+              <li>Contact</li>
+            </a>
+          </ul>
+        </div>
+        <div className="">
+          <h2 className="text-xl font-bold mb-2">Social Media</h2>
+          <ul className="text-3xl flex gap-4">
+            <li>
+              <FaGithub />
+            </li>
+            <li>
+              <FaInstagram />
+            </li>
+            <li>
+              <FaTiktok />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="">
+        <h2 className="text-xl font-bold mb-2">Contact information</h2>
+        <ul>
+          <li>
+            <span className="font-bold">Adress:</span> 123 Road, Stockholm
+            Sweden
+          </li>
+          <li>
+            <span className="font-bold">Telefonnummer:</span> 0123456789
+          </li>
+          <li>
+            <span className="font-bold">Email:</span> test@test.se
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
