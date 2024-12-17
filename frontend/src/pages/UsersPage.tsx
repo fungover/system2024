6628b866577670ed179a783384528604ed5a2885
MickeMarten <123842543+MickeMarten@users.noreply.github.com>
2024-12-17 21:27:15 +0100

    Added try catch to function.

--- frontend/src/pages/UsersPage.tsx
+++ frontend/src/pages/UsersPage.tsx
@@ -1,45 +1,45 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 interface UserPageProps {
     name: string;
     email: string;
 }
 
-
-function UsersPage(){
+function UsersPage() {
     const [users, setUsers] = useState<UserPageProps[]>([]);
+
     async function fetchAllUsers(): Promise<void> {
+        try {
+            const response = await fetch('http://localhost:8080/graphql', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    query: `{
+                        users {
+                            name
+                            email 
+                        }
+                    }`
+                })
+            });
 
-
-  let loot = await fetch('http://localhost:8080/graphql',{
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-          query: `{
-          users {
-          name
-          email 
-          }
-          }`
-      })
-  } )
-        const payload = await loot.json();
-
-        setUsers(payload.data.users);
-
-
+            const payload = await response.json();
+            setUsers(payload.data.users);
+        } catch (error) {
+            console.error("Error fetching users:", error);
+        }
     }
 
     useEffect(() => {
-       fetchAllUsers()
-
+        fetchAllUsers();
     }, []);
-    return(
-        <section>
+
+    return (
+        <section className="">
             <div>
-                <h1>Our daily users!</h1>
+                <h1 className="text-pink-500">Our daily users!</h1>
                 <div>
                     {users.length > 0 ? (
                         <ul>
@@ -50,14 +50,12 @@ function UsersPage(){
                             ))}
                         </ul>
                     ) : (
-                        <p className="animate-spin">Loading users...</p>
+                        <p className="animate-spin">No one uses our application =(</p>
                     )}
                 </div>
-
-
             </div>
         </section>
-    )
+    );
 }
 
 export default UsersPage;
