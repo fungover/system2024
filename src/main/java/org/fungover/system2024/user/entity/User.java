07f077c749e23223c045190a03cea18f7684838f
MikaelaMathisson <mikaela.mathisson@hotmail.se>
2024-12-19 17:44:41 +0100

    testing to see if this fixes sonarcloud gate fail

--- src/main/java/org/fungover/system2024/user/entity/User.java
+++ src/main/java/org/fungover/system2024/user/entity/User.java
@@ -1,13 +1,11 @@
 package org.fungover.system2024.user.entity;
 
 import jakarta.persistence.*;
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.LinkedHashSet;
 import java.util.Set;
 
