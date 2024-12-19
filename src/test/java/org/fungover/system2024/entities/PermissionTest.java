c208f4a261b34394250737f2292b10e1aba1332b
MikaelaMathisson <mikaela.mathisson@hotmail.se>
2024-12-19 16:39:21 +0100

    test commit intellij

--- src/test/java/org/fungover/system2024/entities/PermissionTest.java
+++ src/test/java/org/fungover/system2024/entities/PermissionTest.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PermissionTests {
+class PermissionTest {
 
     private static Validator validator;
     private static ValidatorFactory factory;
