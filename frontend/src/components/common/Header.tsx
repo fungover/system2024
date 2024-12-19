b5595362f6a97a20d8975275eb428e69cefe94b1
Weeezzt <william.vesterberg@lerniastudent.se>
2024-12-19 14:04:18 +0100

    Refactored Header component
    Co-authored-by: AlexanderK88 <AlexanderK88@users.noreply.github.com>

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { FaBars } from "react-icons/fa";
+import MenuItems from "./MenuItems";
+import MenuItemsMobile from "../MenuItemsMobile";
 
 export default function Header() {
   const [isMobile, setIsMobile] = useState(false);
@@ -7,6 +9,21 @@ export default function Header() {
   const handleMobileMenu = () => {
     setIsMobile(!isMobile);
   };
+
+  const menuItems = [
+    {
+      title: "Home",
+      url: "/",
+    },
+    {
+      title: "About",
+      url: "/",
+    },
+    {
+      title: "Contact",
+      url: "/",
+    },
+  ];
 
   return (
     <>
@@ -17,30 +34,7 @@ export default function Header() {
           </div>
           <div className="flex flex-row align-center h-full">
             <div className="hidden md:flex items-center h-full mr-10">
-              <nav className="h-full w-full font-semibold">
-                <ul className="h-full flex gap-8">
-                  <li className="h-full flex items-center border-b-2 border-white hover:border-purple-800">
-                    <a href="#" className="text-black text-xl">
-                      Home
-                    </a>
-                  </li>
-                  <li className="h-full flex items-center border-b-2 border-white hover:border-purple-800">
-                    <a href="#" className="text-black text-xl">
-                      About
-                    </a>
-                  </li>
-                  <li className="h-full flex items-center border-b-2 border-white hover:border-purple-800">
-                    <a href="#" className="text-black text-xl">
-                      Contact
-                    </a>
-                  </li>
-                  <li className="h-full flex items-center">
-                    <button className="p-1 w-24 bg-blue-600 text-white rounded-md shadow-md hover:bg-blue-700">
-                      <p className="text-lg">Login</p>
-                    </button>
-                  </li>
-                </ul>
-              </nav>
+              <MenuItems menuItems={menuItems} />
             </div>
             <div className="flex items-center">
               <img
@@ -63,22 +57,7 @@ export default function Header() {
             : "opacity-0 pointer-events-none -translate-y-full duration-0"
         } transition-all ease-out absolute left-0 right-0 z-10 border-2 border-t-0 shadow-sm bg-white p-6 pt-8 rounded-sm md:hidden`}
       >
-        <ul className="space-y-6 text-lg font-semibold">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-          <li>
-            <button className="p-1 w-full  bg-blue-600 text-white rounded-md shadow-md hover:bg-blue-700 ">
-              Sign in
-            </button>
-          </li>
-        </ul>
+        <MenuItemsMobile menuItemsMobile={menuItems} />
       </div>
     </>
   );
