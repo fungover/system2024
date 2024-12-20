4009ae5fafcc8b320e537a43de31c5c8f63fa407
Conny Pettersson <conny.pettersson@lerniastudent.se>
2024-12-20 13:26:20 +0100

    Revert "# This is the 1st commit message:"
    This reverts commit a612c38fdf2cc5171c129e059737905936590de6.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import UsersPage from "./pages/UsersPage.tsx";
-import LoginPage from './pages/LoginPage';
 import App from './App.tsx'
 import {
     createBrowserRouter,
@@ -21,10 +20,6 @@ const router = createBrowserRouter([
     {
         path:'/users',
         element: <UsersPage/>,
-    },
-    {
-        path: '/auth/login',
-        element: <LoginPage/>
     }
 ]);
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/login': 'http://localhost:8080',
-      '/logout': 'http://localhost:8080',
-      '/oauth2': 'http://localhost:8080',
-      '/api': 'http://localhost:8080'
-    }
-  },
   build: {
     outDir: '../src/main/resources/static/',
     emptyOutDir: true,
