483c8974f1450266fb5b3dc88a2bc532bf17518b
AnnaLenaO <annalenao@me.com>
2025-05-03 19:26:26 +0200

    update User credentials GraphQL Mutation

--- src/main/java/org/fungover/system2024/user/UserController.java
+++ src/main/java/org/fungover/system2024/user/UserController.java
@@ -1,18 +1,29 @@
 package org.fungover.system2024.user;
 
 import org.fungover.system2024.user.dto.UserDto;
+import org.fungover.system2024.user.dto.UserUpdateDto;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController {
 
-  private final UserService userService;
+    private final UserService userService;
 
-  public UserController(UserService userService) {
-    this.userService = userService;
-  }
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-  @QueryMapping
-  Iterable<UserDto> users(){ return userService.getAllUsers(); }
+    @QueryMapping
+    Iterable<UserDto> users() {
+        return userService.getAllUsers();
+    }
+
+    @MutationMapping
+    public Boolean updateUser(@Argument("Input") UserUpdateDto input) {
+        userService.updateUser(input);
+        return true;
+    }
 }
--- src/main/java/org/fungover/system2024/user/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package org.fungover.system2024.user.repository;
-
-import org.fungover.system2024.user.entity.Role;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface RoleRepository extends ListCrudRepository<Role, Integer> {
-}
