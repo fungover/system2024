479f7f923843acef4fcee548f13484771374fddc
rl-86 <rl86@live.se>
2025-09-16 00:17:48 +0200

    Update Header navigation to use Router Links. Restructure routes to share Header/Footer layout.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
+import HomePage from './pages/HomePage.tsx'
 import UsersPage from "./pages/UsersPage.tsx";
 import App from './App.tsx'
 import LoginPage from "./pages/LoginPage.tsx";
@@ -11,18 +12,20 @@ const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
-    },
-    {
-        path: '/cake',
-        element: <div style={{ fontSize: 150 }}>🍰</div>,
-    },
-    {
-        path:'/users',
-        element: <UsersPage/>,
-    },
-    {
-        path: '/auth/login',
-        element: <LoginPage/>
+        children: [
+            {
+                index: true,
+                element: <HomePage />
+            },
+            {
+                path: 'login',
+                element: <LoginPage />
+            },
+            {
+                path: 'users',
+                element: <UsersPage />
+            }
+        ]
     }
 ]);
 
@@ -31,5 +34,3 @@ createRoot(document.getElementById('root')!).render(
         <RouterProvider router={router} />
     </StrictMode>
 );
-
-
